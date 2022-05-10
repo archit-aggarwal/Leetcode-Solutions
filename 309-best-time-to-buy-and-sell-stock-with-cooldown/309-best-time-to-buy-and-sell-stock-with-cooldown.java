4c40ba488ecef8b38f1f9cc86c99d2ea0dbf710a
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-05-10 22:49:09 +0530

    Time: 1 ms (86.57%), Space: 41.7 MB (73.66%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -2,19 +2,19 @@ class Solution {
     public int maxProfit(int[] prices) {
          if(prices.length <= 1) return 0;
         
-        int[] buy = new int[prices.length];
-        int[] sell = new int[prices.length];
-        
-        buy[0] = -prices[0];
-        sell[0] = 0;
-        buy[1] = Math.max(-prices[0], -prices[1]); // Either buy 0th stock or 1st stock
-        sell[1] = Math.max(0, prices[1] - prices[0]); // Either do nothing or Buy 0 Sell 1
+        int buy0 = -prices[0];
+        int sell0 = 0;
+        int buy1 = Math.max(-prices[0], -prices[1]); // Either buy 0th stock or 1st stock
+        int sell1 = Math.max(0, prices[1] - prices[0]); // Either do nothing or Buy 0 Sell 1
          
         for(int i=2; i<prices.length; i++){
-            buy[i] = Math.max(buy[i - 1], sell[i - 2] - prices[i]);
-            sell[i] = Math.max(sell[i - 1], buy[i - 1] + prices[i]);
+            int newbuy = Math.max(buy1, sell0 - prices[i]);
+            int newsell = Math.max(sell1, buy1 + prices[i]);
+            
+            buy0 = buy1; sell0 = sell1;
+            buy1 = newbuy; sell1 = newsell;
         }
         
-        return sell[prices.length - 1];
+        return sell1;
     }
 }
