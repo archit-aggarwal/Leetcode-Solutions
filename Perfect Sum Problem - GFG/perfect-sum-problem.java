5d62d7a9860129fba3051283a4e25a8e07dd91d6
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-05-11 22:54:03 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.java
+++ Perfect Sum Problem - GFG/perfect-sum-problem.java
@@ -27,18 +27,22 @@ class GfG
 class Solution{
     public int perfectSum(int arr[],int n, int target) 
 	{ 
-	    int[][] dp = new int[n + 1][target + 1];
-	    dp[0][0] = 1; // Empty Subset to form 0 Target
+	    int[] dp = new int[target + 1];
+	    dp[0] = 1; // Empty Subset to form 0 Target
 	    
 	    for(int i=1; i<=n; i++){
+	        int[] newdp = new int[target + 1];
+	        
 	        for(int j=0; j<=target; j++){
-	            int no = dp[i - 1][j]; // No Call
-	            int yes = (j >= arr[i - 1]) ? dp[i - 1][j - arr[i - 1]] : 0;
+	            int no = dp[j]; // No Call
+	            int yes = (j >= arr[i - 1]) ? dp[j - arr[i - 1]] : 0;
 	            
-	            dp[i][j] = (no + yes) % 1000000007;
+	            newdp[j] = (no + yes) % 1000000007;
 	        }
+	        
+	        dp = newdp;
 	    }
 	    
-	    return dp[n][target];
+	    return dp[target];
 	} 
 }
