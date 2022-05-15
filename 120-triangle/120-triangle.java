184fcac35255a2e8fefcde58cf59464d5447f81b
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-05-15 11:51:58 +0530

    Time: 2 ms (92.45%), Space: 44.1 MB (42.92%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,11 +1,11 @@
 class Solution {
-    public int helper(List<List<Integer>> triangle, int row, int col, int[][] dp){
+    public int helper(List<List<Integer>> triangle, int row, int col, Integer[][] dp){
         if(row == triangle.size() - 1){
             // Entire Last Row is the Destination
             return triangle.get(row).get(col);
         }
         
-        if(dp[row][col] != Integer.MIN_VALUE) return dp[row][col];
+        if(dp[row][col] != null) return dp[row][col];
         
         int down = helper(triangle, row + 1, col, dp);
         int rightdown = helper(triangle, row + 1, col + 1, dp);
@@ -15,13 +15,7 @@ class Solution {
     
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int[][] dp = new int[n + 1][n + 1];  
-        for(int i=0; i<=n; i++){
-            for(int j=0; j<=n; j++){
-                dp[i][j] = Integer.MIN_VALUE;
-            }
-        }
-        
+        Integer[][] dp = new Integer[n + 1][n + 1];
         return helper(triangle, 0, 0, dp);
     }
 }
