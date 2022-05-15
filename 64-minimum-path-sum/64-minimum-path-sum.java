9b25fa217f28a0336620d97df8858b3a6d5081b4
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-05-15 10:34:59 +0530

    Time: 3 ms (53.74%), Space: 46.5 MB (19.42%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.java
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -1,29 +1,40 @@
 class Solution {
-    public int helper(int row, int col, int[][] grid, int[][] dp){
-        // Negative Base Case: Out of the Grid
-        if(row >= grid.length || col >= grid[0].length) 
-            return Integer.MAX_VALUE;
+//     public int helper(int row, int col, int[][] grid, int[][] dp){
+//         // Negative Base Case: Out of the Grid
+//         if(row >= grid.length || col >= grid[0].length) 
+//             return Integer.MAX_VALUE;
                 
-        // Positive Base Case: Destination is Reached
-        if(row == grid.length - 1 && col == grid[0].length - 1)
-            return grid[row][col];
+//         // Positive Base Case: Destination is Reached
+//         if(row == grid.length - 1 && col == grid[0].length - 1)
+//             return grid[row][col];
                 
-        if(dp[row][col] != -1) return dp[row][col];
+//         if(dp[row][col] != -1) return dp[row][col];
 
-        int horizontal = helper(row, col + 1, grid, dp); 
-        int vertical = helper(row + 1, col, grid, dp);
+//         int horizontal = helper(row, col + 1, grid, dp); 
+//         int vertical = helper(row + 1, col, grid, dp);
         
-        return dp[row][col] = Math.min(horizontal, vertical) + grid[row][col];
-    }
+//         return dp[row][col] = Math.min(horizontal, vertical) + grid[row][col];
+//     }
     
     public int minPathSum(int[][] grid) {
             int[][] dp = new int[grid.length + 1][grid[0].length + 1];
             for(int i=0; i<=grid.length; i++){
                 for(int j=0; j<=grid[0].length; j++){
-                    dp[i][j] = -1;
+                    dp[i][j] = Integer.MAX_VALUE;
                 }
             }
             
-            return helper(0, 0, grid, dp);
+            for(int i=grid.length-1; i>=0; i--){
+                for(int j=grid[0].length-1; j>=0; j--){
+                    if(i == grid.length - 1 && j == grid[0].length - 1){
+                        dp[i][j] = grid[i][j];
+                        continue;
+                    }
+                    
+                    dp[i][j] = Math.min(dp[i + 1][j], dp[i][j + 1]) + grid[i][j];
+                }
+            }
+        
+            return dp[0][0];
     }
 }
