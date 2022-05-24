81f0db7fd558799d4d46cec6fb7a4e7e0dc0a983
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-05-24 22:57:15 +0530

    Time: 2 ms (98.32%), Space: 52 MB (48.53%) - LeetHub

--- 845-longest-mountain-in-array/845-longest-mountain-in-array.java
+++ 845-longest-mountain-in-array/845-longest-mountain-in-array.java
@@ -1,32 +1,29 @@
 class Solution {
     public int longestMountain(int[] nums) {
-        int[] left = new int[nums.length];
-        // Longest increasing subarray ending at index i
-        Arrays.fill(left, 1);
-        
-        for(int i=1; i<nums.length; i++){
-            if(nums[i - 1] < nums[i]){
-                left[i] = left[i - 1] + 1;
-            }
-        }
-        
-        
-        int[] right = new int[nums.length];
-        // Longest decreasing subarray starting at index i
-        Arrays.fill(right, 1);
-        
-        for(int i=nums.length-2; i>=0; i--){
-            if(nums[i] > nums[i + 1]){
-                right[i] = right[i + 1] + 1;
-            }
-        }
-        
-        // Longest Bitonic Subarray = LIS + LDS - 1
         int max = 0;
+        
         for(int i=0; i<nums.length; i++){
-            if(left[i] > 1 && right[i] > 1){
-                max = Math.max(max, left[i] + right[i] - 1);
+            
+            // Potential Peak Element
+            if(i > 0 && i < nums.length - 1 && nums[i] > nums[i - 1] && nums[i] > nums[i + 1]){
+                
+                int curr = 1;
+                
+                int left = i - 1;
+                while(left >= 0 && nums[left] < nums[left + 1]){
+                    left--;
+                    curr++;
+                }
+                
+                int right = i + 1;
+                while(right < nums.length && nums[right] < nums[right - 1]){
+                    right++;
+                    curr++;
+                }
+                
+                max = Math.max(max, curr);
             }
+            
         }
         
         return max;
