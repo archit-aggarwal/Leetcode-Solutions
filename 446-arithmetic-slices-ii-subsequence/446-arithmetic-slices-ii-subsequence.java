5fbed624136f8193ff4abb9be23a40594812da12
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-05-25 23:57:28 +0530

    Time: 404 ms (12.50%), Space: 182 MB (5.63%) - LeetHub

---
+++ 446-arithmetic-slices-ii-subsequence/446-arithmetic-slices-ii-subsequence.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        if(nums.length < 3) return 0;
+        
+        // DP[IDX][COMMON DIFF] = COUNT OF AP SUBSETS
+        HashMap<Long, Long>[] dp = new HashMap[nums.length];
+        for(int i=0; i<nums.length; i++){
+            dp[i] = new HashMap<>();
+        }
+        
+        long count = 0;
+        for(int i=1; i<nums.length; i++){
+            for(int j=i-1; j>=0; j--){
+                long diff = 1l * nums[i] - nums[j];
+                
+                long oldVal = dp[i].getOrDefault(diff, 0l);
+                long newVal = oldVal + dp[j].getOrDefault(diff, 0l) + 1l;
+                
+                dp[i].put(diff, newVal);
+                count += dp[j].getOrDefault(diff, 0l);
+            }
+        }
+        
+        return (int)count;
+    }
+}
