c304e515d45b3b5e59c27a7509a8a891e669f5bf
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-05-25 22:35:03 +0530

    Time: 51 ms (57.48%), Space: 43.6 MB (42.65%) - LeetHub

---
+++ 279-perfect-squares/279-perfect-squares.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        
+        for(int i=1; i<=n; i++){
+            dp[i] = i;
+            for(int j=1; j*j<=i; j++){
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        
+        return dp[n];
+    }
+}
