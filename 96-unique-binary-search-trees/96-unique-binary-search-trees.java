e57aa89cd8f9381b868e5e8b86cc2e887b5a1417
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-06-02 18:52:30 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (61.03%) - LeetHub

---
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 1;
+        
+        for(int i=2; i<=n; i++)
+            for(int j=0; j<i; j++)
+                dp[i] += dp[j] * dp[i - j - 1];
+        
+        return dp[n];
+    }
+}
