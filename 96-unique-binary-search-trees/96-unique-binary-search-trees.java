3365940ee38b13c08a2418c5588ee0dae4481ed4
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-06-04 14:24:12 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (60.92%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -1,25 +1,20 @@
 class Solution {
-    public int catalan(int n, int[] dp){
-        if(n == 0 || n == 1) return 1;
-        if(dp[n] != -1) return dp[n];
-        
-        int ans = 0;
-        for(int i=0; i<n; i++){
-            ans = ans + catalan(i, dp) * catalan(n - 1 - i, dp);
+    long ncr(int n, int k) // Function to calculate Ci(n.k)
+    {
+        long res = 1;
+        if (k > n - k)
+            k = n - k;
+        // Since Ci(n,k) = Ci(n,n-k), property of binomial coefficients
+
+        for (int i = 0; i < k; i++) {
+            res *= (n - i);
+            res /= (i + 1);
         }
-        
-        return dp[n] = ans;
+
+        return res;
     }
-    
-    public int numTrees(int n) {
-        int[] dp = new int[n + 1];
-        // Arrays.fill(dp, -1);
-        // return catalan(n, dp);
-        
-        dp[0] = dp[1] = 1;
-        for(int i=2; i<=n; i++)
-            for(int j=0; j<i; j++)
-                dp[i] += dp[j] * dp[i - j - 1];
-        return dp[n];
+
+    int numTrees(int n) {
+        return (int) (ncr(2 * n, n) / (n + 1));
     }
 }
