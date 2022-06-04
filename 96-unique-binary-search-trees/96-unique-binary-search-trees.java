aed4928807d1a8303592efed3ffc61ece1b66d79
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-06-04 09:44:28 +0530

    Time: 2391 ms (5.00%), Space: 39.2 MB (76.58%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -1,12 +1,14 @@
 class Solution {
+    public int catalan(int n){
+        if(n == 0 || n == 1) return 1;
+        
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            ans = ans + catalan(i) * catalan(n - 1 - i);
+        }
+        return ans;
+    }
     public int numTrees(int n) {
-        int[] dp = new int[n + 1];
-        dp[0] = dp[1] = 1;
-        
-        for(int i=2; i<=n; i++)
-            for(int j=0; j<i; j++)
-                dp[i] += dp[j] * dp[i - j - 1];
-        
-        return dp[n];
+        return catalan(n);
     }
 }
