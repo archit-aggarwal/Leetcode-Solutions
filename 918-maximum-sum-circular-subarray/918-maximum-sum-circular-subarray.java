14fe5497ac4ae3ddd6b2e4a7927786ebdf4467fb
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-06-05 11:06:30 +0530

    Time: 5 ms (60.28%), Space: 63.3 MB (63.07%) - LeetHub

--- 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.java
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.java
@@ -1,34 +1,25 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
-        int currSum = 0, maxSum = Integer.MIN_VALUE;
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+        int total = 0;
+        
         for(int i=0; i<nums.length; i++){
-            currSum = Math.max(currSum + nums[i], nums[i]);
+            currSum = Math.max(nums[i], currSum + nums[i]);
+            total += nums[i];
             maxSum = Math.max(maxSum, currSum);
         }
         
-        int prefSum = nums[0];
-        int[] prefix = new int[nums.length];
-        prefix[0] = prefSum;
-        
-        for(int i=1; i<nums.length; i++){
-            prefSum += nums[i];
-            prefix[i] = Math.max(prefix[i - 1], prefSum);
-        }
-        
-        int suffixSum = nums[nums.length - 1];
-        int[] suffix = new int[nums.length];
-        suffix[nums.length - 1] = suffixSum;
-        
-        for(int i=nums.length-2; i>=0; i--){
-            suffixSum += nums[i];
-            suffix[i] = Math.max(suffixSum, suffix[i + 1]);
-        }
+        int minSum = Integer.MAX_VALUE;
+        currSum = 0;
         
         for(int i=0; i<nums.length; i++){
-            suffixSum = (i + 1 < nums.length) ? suffix[i + 1] : 0;
-            maxSum = Math.max(maxSum, prefix[i] + suffixSum);
+            currSum = Math.min(nums[i], currSum + nums[i]);
+            minSum = Math.min(currSum, minSum);
         }
         
-        return maxSum;
+        if(maxSum > 0) 
+            return Math.max(maxSum, total - minSum);
+        else return maxSum;
     }
 }
