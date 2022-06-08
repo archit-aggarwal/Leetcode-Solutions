e0454a8b60dc97c5804e36d33f371e5f17af15db
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-06-08 23:44:06 +0530

    Time: 1 ms (77.14%), Space: 41.3 MB (56.37%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.java
+++ 486-predict-the-winner/486-predict-the-winner.java
@@ -4,12 +4,8 @@ class Solution {
         if(left == right) return nums[left];
         if(dp[left][right] != -1) return dp[left][right];
         
-        int c1 = helper(left + 2, right, nums, dp);
-        int c2 = helper(left + 1, right - 1, nums, dp);
-        int c3 = helper(left, right - 2, nums, dp);
-        
-        return dp[left][right] = Math.max(nums[left] + Math.min(c1, c2), 
-                                          nums[right] + Math.min(c2, c3));
+        return dp[left][right] = Math.max(nums[left] - helper(left + 1, right, nums, dp), 
+                                          nums[right] - helper(left, right - 1, nums, dp));
     }
     
     public boolean PredictTheWinner(int[] nums) {
@@ -19,16 +15,8 @@ class Solution {
                 dp[i][j] = -1;
         
         
-        int ascore = helper(0, nums.length - 1, nums, dp);
-        
-        int total = 0;
-        for(int i=0; i<nums.length; i++){
-            total = total + nums[i];
-        }
-        
-        int bscore = total - ascore;
-        
-        if(ascore >= bscore) return true;
+        int diff = helper(0, nums.length - 1, nums, dp);
+        if(diff >= 0) return true;
         return false;
     }
 }
