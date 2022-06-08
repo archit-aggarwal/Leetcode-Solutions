2cc6280163a1ee3f3e5bbae3a365d8491f8444f4
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-06-08 23:24:58 +0530

    Time: 1 ms (77.14%), Space: 40.7 MB (72.51%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.java
+++ 486-predict-the-winner/486-predict-the-winner.java
@@ -1,17 +1,25 @@
 class Solution {
-    public int helper(int left, int right, int[] nums){
+    public int helper(int left, int right, int[] nums, int[][] dp){
         if(left > right) return 0;
         if(left == right) return nums[left];
+        if(dp[left][right] != -1) return dp[left][right];
         
-        int c1 = helper(left + 2, right, nums);
-        int c2 = helper(left + 1, right - 1, nums);
-        int c3 = helper(left, right - 2, nums);
+        int c1 = helper(left + 2, right, nums, dp);
+        int c2 = helper(left + 1, right - 1, nums, dp);
+        int c3 = helper(left, right - 2, nums, dp);
         
-        return Math.max(nums[left] + Math.min(c1, c2), nums[right] + Math.min(c2, c3));
+        return dp[left][right] = Math.max(nums[left] + Math.min(c1, c2), 
+                                          nums[right] + Math.min(c2, c3));
     }
     
     public boolean PredictTheWinner(int[] nums) {
-        int ascore = helper(0, nums.length - 1, nums);
+        int[][] dp = new int[nums.length + 1][nums.length + 1];
+        for(int i=0; i<dp.length; i++)
+            for(int j=0; j<dp[0].length; j++)
+                dp[i][j] = -1;
+        
+        
+        int ascore = helper(0, nums.length - 1, nums, dp);
         
         int total = 0;
         for(int i=0; i<nums.length; i++){
