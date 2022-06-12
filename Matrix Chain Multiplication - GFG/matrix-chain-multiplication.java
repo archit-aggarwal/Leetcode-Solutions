f55a78fd0cf219723e87092419de254279666796
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-06-12 09:44:13 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.java
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.java
@@ -49,10 +49,26 @@ class Solution{
     
     static int matrixMultiplication(int N, int nums[]) {
         int[][] dp = new int[N + 1][N + 1];
-        for(int i=0; i<=N; i++)
-            for(int j=0; j<=N; j++)
-                dp[i][j] = -1;
+        
+        for(int gap=1; gap<N-1; gap++){
+            for(int l=0, r=gap; r<N-1; l++, r++){
                 
-        return helper(0, N - 2, nums, dp);
+                int minCost = Integer.MAX_VALUE;
+                // Creating partitions
+                for(int k=l; k<r; k++){
+                    // Left Matrices -> Multiplication Min Cost
+                    int left = dp[l][k];
+                    // Right Matrices -> Multiplication Min Cost
+                    int right = dp[k + 1][r];
+                    // Left * Right Multiplication Cost
+                    int cost = left + (nums[l] * nums[k + 1] * nums[r + 1]) + right;
+                    minCost = Math.min(minCost, cost);
+                }
+                
+                dp[l][r] = minCost;
+            }
+        }
+        
+        return dp[0][N - 2];
     }
 }
