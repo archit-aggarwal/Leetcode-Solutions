22f4b5de0ef84355d8af923c30e5eef2ea16ea51
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-06-16 22:44:42 +0530

    Time: 11 ms (49.74%), Space: 47.6 MB (18.97%) - LeetHub

---
+++ 139-word-break/139-word-break.java
@@ -0,0 +1,26 @@
+class Solution {
+    HashSet<String> dict;
+    
+    public int wordBreak(int i, String s, int[] dp){
+        if(i == s.length()) return 1;
+        if(dp[i] != -1) return dp[i];
+        
+        for(int j=i; j<s.length(); j++){
+            String prefix = s.substring(i, j + 1);
+            if(dict.contains(prefix) == true && wordBreak(j + 1, s, dp) == 1)
+                return dp[i] = 1;
+        }
+        
+        return dp[i] = 0;
+    }
+    
+    public boolean wordBreak(String s, List<String> wordDict) {
+        dict = new HashSet<>();
+        int[] dp = new int[s.length() + 1];
+        Arrays.fill(dp, -1);
+        
+        for(String str: wordDict) dict.add(str);
+        
+        return (wordBreak(0, s, dp) == 1) ? true : false;    
+    }
+}
