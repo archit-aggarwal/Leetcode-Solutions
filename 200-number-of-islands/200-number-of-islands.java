fbdb7df0d111e2eae87ca424befefa34530ebf7b
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-07-03 11:31:39 +0530

    Time: 7 ms (33.61%), Space: 57.7 MB (36.73%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,14 +1,15 @@
 class Solution {
+    int[][] directions = {{+1, 0}, {-1, 0}, {0, +1}, {0, -1}};
+    
     public void DFS(int r, int c, char[][] grid){
         // Out of Matrix, Water Cell, Visited Land
         if(r < 0 || c < 0 || r >= grid.length || c >= grid[0].length 
            || grid[r][c] == '0' || grid[r][c] == 'N') return;
         
         grid[r][c] = 'N'; // Visited Land
-        DFS(r - 1, c, grid);
-        DFS(r + 1, c, grid);
-        DFS(r, c - 1, grid);
-        DFS(r, c + 1, grid);
+        for(int[] direction: directions){
+            DFS(r + direction[0], c + direction[1], grid);
+        }
     }
     
     public int numIslands(char[][] grid) {
