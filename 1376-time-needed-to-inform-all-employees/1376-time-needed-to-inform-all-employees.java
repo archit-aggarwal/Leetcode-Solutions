a420e609d4eb72de8eae211c960fd2c91267fefb
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-07-05 23:38:37 +0530

    Time: 146 ms (58.41%), Space: 123.5 MB (57.01%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -0,0 +1,50 @@
+class Pair{
+    int nbr;
+    int wt;
+    Pair(int nbr, int wt){
+        this.nbr = nbr; this.wt = wt;
+    }
+}
+
+class Graph{
+    ArrayList<Pair>[] adj;
+    Graph(int n){
+        adj = new ArrayList[n];
+        for(int i=0; i<n; i++){
+            adj[i] = new ArrayList<>();
+        }
+    }
+    
+    public void addEdge(int src, int nbr, int wt){
+        // Directed And Weighted
+        adj[src].add(new Pair(nbr, wt));
+    }
+}
+
+class Solution {
+    public int DFS(int src, int time, Graph g){
+        int maxTime = time;
+        for(Pair p: g.adj[src]){
+            maxTime = Math.max(maxTime, DFS(p.nbr, time + p.wt, g));
+        }
+        return maxTime;
+    }
+    
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        Graph g = new Graph(n);
+        
+        int time = 0;
+        
+        for(int i=0; i<n; i++){
+            if(manager[i] == -1){
+                time = informTime[i];
+            } else {
+                // manager[i] -> parent, i -> Child
+                g.addEdge(manager[i], i, informTime[i]);
+            }
+            
+        }
+        
+        return DFS(headID, time, g);
+    }
+}
