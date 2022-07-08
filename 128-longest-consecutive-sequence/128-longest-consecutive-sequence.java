6dc3a2f037f727e58df0ce8d5403ffdec0b1126c
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-07-08 21:18:54 +0530

    Time: 813 ms (6.79%), Space: 159 MB (11.70%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -1,58 +1,24 @@
 class Solution {
-    public int twoPointer(int[] nums){
-        if(nums.length == 0) return 0;
-        
-        Arrays.sort(nums);
-        
-        int left = 0, max = 1;
-        while(left < nums.length){
-            
-            int right = left + 1, count = 1;
-            while(right < nums.length && nums[right] <= nums[right - 1] + 1){
-                if(nums[right] == nums[right - 1] + 1)
-                    count++;
-                right++;
-            }
-            
-            max = Math.max(max, count);
-            left = right;
-        }
-        
-        return max;
-    }
-    
     public int longestConsecutive(int[] nums) {
-        if(nums.length == 0) return 0;
-        
-        // Unique Key
-        HashMap<Integer, Boolean> isStarting = new HashMap<>();
-       
-        // 1. Take All Elements as Potential Starting Sequence
+        HashMap<Integer, Integer> freq = new HashMap<>();
         for(int i=0; i<nums.length; i++){
-            isStarting.put(nums[i], true);
-        }
-        
-        // 2. Remove Elements which cannot start a new Sequence
-        for(int i=0; i<nums.length; i++){
-            if(isStarting.containsKey(nums[i] - 1) == true){
-                // We Will Join the Previous Sequence, 
-                // Hence, we cannot start the sequence
-                isStarting.put(nums[i], false);
+            if(freq.containsKey(nums[i]) == true){
+                freq.put(nums[i], freq.get(nums[i]) + 1);
+            } else {
+                freq.put(nums[i], 1);
             }
         }
         
         int max = 0;
-        for(int start: isStarting.keySet()){
-            
-            if(isStarting.get(start) == true){
-                int curr = 1;
-                for(int end=start+1; isStarting.containsKey(end) == true; end++){
-                    curr++;
+        for(int i=0; i<nums.length; i++){
+            // If a Starting Element of a Sequence -> Figure out sequence length
+            if(freq.containsKey(nums[i] - 1) == false){
+                int length = 0, ptr = nums[i];
+                while(freq.containsKey(ptr) == true){
+                    length++; ptr++;
                 }
-
-                max = Math.max(max, curr);
+                max = Math.max(max, length);
             }
-            
         }
         
         return max;
