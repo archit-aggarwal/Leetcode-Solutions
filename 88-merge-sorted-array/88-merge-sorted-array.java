43c56ec5bd051ca899b7df6f1d5e7bf2092a92d4
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-07-09 19:45:49 +0530

    Time: 0 ms (100.00%), Space: 42 MB (93.60%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p3 = nums1.length - 1;
+        int p1 = m - 1, p2 = n - 1;
+        
+        while(p1 >= 0 && p2 >= 0){
+            if(nums1[p1] > nums2[p2]){
+                nums1[p3] = nums1[p1];
+                p1--; p3--;
+            } else {
+                nums1[p3] = nums2[p2];
+                p2--; p3--;
+            }
+        }
+        
+        while(p1 >= 0){
+            nums1[p3] = nums1[p1];
+            p1--; p3--;   
+        }
+        
+        while(p2 >= 0){
+            nums1[p3] = nums2[p2];
+            p2--; p3--;
+        }
+    }
+}
