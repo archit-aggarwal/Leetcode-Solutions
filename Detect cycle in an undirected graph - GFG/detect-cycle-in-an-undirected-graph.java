6ef984abb5726b2f462e446d3911f66fa6e6c7d3
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-07-10 16:22:43 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -32,13 +32,55 @@ class GFG {
 
 
 class Solution {
-    public boolean DFS(int src, int par, ArrayList<ArrayList<Integer>> adj, boolean[] vis){
-        if(vis[src] == true) return true;
+    public boolean DFS(int src, int par, ArrayList<ArrayList<Integer>> adj, boolean[] vis, int[] parent){
+        parent[src] = par;
+        if(vis[src] == true){
+            
+            int ptr = src;
+            do{
+               System.out.print(ptr);
+               ptr = parent[ptr];
+            } while(ptr != src);
+            
+            System.out.println(src);
+            
+            return true;
+        }
         
         vis[src] = true;
         for(Integer nbr: adj.get(src)){
-            if(nbr != par && DFS(nbr, src, adj, vis) == true) 
+            if(nbr != par && DFS(nbr, src, adj, vis, parent) == true) 
                 return true;
+        }
+        
+        return false;
+    }
+    
+    public static class Pair{
+        int src;
+        int par;
+        
+        Pair(int src, int par){
+            this.src = src;
+            this.par = par;
+        }
+    }
+    
+    public boolean BFS(int src, ArrayList<ArrayList<Integer>> adj, boolean[] vis){
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(src, -1));
+        
+        while(q.size() > 0){
+            Pair front = q.remove();
+            src = front.src;
+            int par = front.par;
+            
+            if(vis[src] == true) return true; // Cycle Detected
+            vis[src] = true;
+            
+            for(Integer nbr: adj.get(src)){
+                if(nbr != par) q.add(new Pair(nbr, src));
+            }
         }
         
         return false;
@@ -46,10 +88,9 @@ class Solution {
     
     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
         boolean[] vis = new boolean[V];
+        
         for(int i=0; i<V; i++){
-            if(vis[i] == false && DFS(i, -1, adj, vis) == true){
-                return true;
-            }
+            if(vis[i] == false && BFS(i, adj, vis) == true) return true;
         }
         
         return false;
