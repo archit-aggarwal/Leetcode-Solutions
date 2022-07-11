84a31a059a823e03209d09e81d2b6ebcb6035089
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-07-11 21:27:03 +0530

    Time: 5 ms (71.50%), Space: 44.2 MB (75.37%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,26 +1,33 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-//         PriorityQueue<Integer> q = new PriorityQueue<>();
-//         // Minimum Heap
+        // PriorityQueue<Integer> q = new PriorityQueue<>();
+        // Minimum Heap
         
-//         for(int i=0; i<nums.length; i++) q.add(nums[i]);
+        // for(int i=0; i<nums.length; i++) q.add(nums[i]);
         
-//         // Heap Sort -> O(N log N)
-//         int idx = 0;
-//         while(q.size() > 0){
-//             nums[idx] = q.remove();
-//             idx++;
-//         }
+        // Heap Sort -> O(N log N)
+        // int idx = 0;
+        // while(q.size() > 0){
+        //     nums[idx] = q.remove();
+        //     idx++;
+        // }
         
-//         return nums[nums.length - k];
+        // return nums[nums.length - k];
         
-        PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
         // Maximum Heap
+        // PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+        // for(int i=0; i<nums.length; i++) q.add(nums[i]);
+        // for(int i=0; i<k-1; i++) q.remove();
+        // return q.peek();
         
-        for(int i=0; i<nums.length; i++) q.add(nums[i]);
+        PriorityQueue<Integer> q = new PriorityQueue<>();
         
-        for(int i=0; i<k-1; i++){
-            q.remove();
+        for(int i=0; i<nums.length; i++){
+            if(i < k) q.add(nums[i]);
+            else if(q.peek() < nums[i]){
+                q.remove();
+                q.add(nums[i]);
+            } 
         }
         
         return q.peek();
