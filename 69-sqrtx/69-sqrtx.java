d5b197d8f8d1a5ed7ef157855884c6750dd46930
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-07-18 20:50:55 +0530

    Time: 3 ms (36.68%), Space: 41.3 MB (51.89%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,25 +1,22 @@
 class Solution {
     public int mySqrt(int x) {
-        // Approach 1
-        // return (int)Math.pow(x, 0.5);
-        
-        // Approach 2
-        // return (int)Math.sqrt(x);
-        
-        // Approach 3:
-        // Binary Search - O(log2 x)
         if(x == 0 || x == 1) return x;
+        // return (int)Math.sqrt(x);
+        long left = 1l, right = x;
+        long ans = 1l;
         
-        int left = 1, right = x, ans = 1;
         while(left <= right){
-            int mid = left + (right - left) / 2;
-            if((long)mid * mid <= x){
+            long mid = left + (right - left) / 2l;
+            // int mid = (left + right) / 2;
+            
+            if(mid * mid <= x){
                 ans = mid;
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
-        return ans;
+        
+        return (int)ans;
     }
 }
