218edfb743d36e752eb8ea6c0792248dd6ea20d2
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-07-23 17:08:13 +0530

    Time: 307 ms (10.19%), Space: 249 MB (5.00%) - LeetHub

---
+++ 815-bus-routes/815-bus-routes.java
@@ -0,0 +1,71 @@
+class Solution {
+    public static class Pair implements Comparable<Pair>{
+        int busStop; // NODE
+        int busColor; // COLOR (EDGE)
+        int busChanges;
+        
+        Pair(int busStop, int busColor, int busChanges){
+            this.busStop = busStop;
+            this.busColor = busColor;
+            this.busChanges = busChanges;
+        }
+        
+        public int compareTo(Pair other){
+            return this.busChanges - other.busChanges;
+        }
+    }
+    public int numBusesToDestination(int[][] routes, int source, int target) {
+        if(source == target) return 0;
+        
+        int n = 0;
+        for(int[] route: routes)
+            for(int stop: route)
+                n = Math.max(n, stop);
+        
+        ArrayList<Pair>[] adj = new ArrayList[n + 1];
+        for(int i=0; i<=n; i++) adj[i] = new ArrayList<>();
+        
+        // Loop on All Buses (Routes)
+        for(int i=0; i<routes.length; i++){
+            for(int j=0; j<routes[i].length; j++){
+                int src = routes[i][j];
+                int dest = routes[i][(j + 1) % routes[i].length];
+                
+                adj[src].add(new Pair(dest, i, 0));
+                adj[dest].add(new Pair(src, i, 0));
+            }
+        }
+        
+        PriorityQueue<Pair> q = new PriorityQueue<>();
+        // q.add(new Pair(source, -1, 0));
+        for(int i=0; i<adj[source].size(); i++){
+            q.add(new Pair(source, adj[source].get(i).busColor, 1));
+        }
+        
+        int[][] vis = new int[n + 1][routes.length + 1];
+        for(int i=0; i<=n; i++){
+         for(int j=0; j<routes.length; j++)
+             vis[i][j] = -1;
+        }
+        
+        if(adj[source].size() == 0) return -1; // No Bus from Source
+        
+        while(q.size() > 0){
+            Pair top = q.remove();
+            if(top.busStop == target) return top.busChanges;
+            
+            if(vis[top.busStop][top.busColor] != -1) continue;
+            vis[top.busStop][top.busColor] = top.busChanges;
+            
+            for(Pair nbr: adj[top.busStop]){
+                if(top.busColor == nbr.busColor){
+                    q.add(new Pair(nbr.busStop, nbr.busColor, top.busChanges));
+                } else {
+                    q.add(new Pair(nbr.busStop, nbr.busColor, top.busChanges + 1));
+                }
+            }
+        }
+        
+        return -1;
+    }
+}
