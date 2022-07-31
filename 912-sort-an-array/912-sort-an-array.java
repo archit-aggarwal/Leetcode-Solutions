7b9e5656e02f4c6e1b29a8d147bf1563ca72b145
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-07-31 11:41:35 +0530

    Time: 10 ms (92.85%), Space: 68.8 MB (52.98%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,42 +1,31 @@
 class Solution {
-    public int partition(int[] arr, int left, int right, int pivot){
-        int low = left, high = left;
+    public static void countSort(int[] arr, int min, int max) {
+        int[] freq = new int[max - min + 1];
 
-        while(high <= right){
+        for(int i=0; i<arr.length; i++){
+            int bucket = arr[i] - min;
+            freq[bucket]++;
+        }
 
-            if(arr[high] > pivot){
-                high++;
-            } else {
-                swap(arr, high, low);
-                low++; high++;
+
+        int idx = 0;
+        for(int bucket=0; bucket<freq.length; bucket++){
+            int val = bucket + min;
+            while(freq[bucket]-- > 0){
+                arr[idx] = val;
+                idx++;
             }
         }
-        
-        return low - 1;
-    }
-    
-    public void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-    
-    public void randomize(int[] nums, int left, int right){
-        int randomIdx = (int)(Math.random()*(right-left+1)+left);
-        swap(nums, randomIdx, right);
-    }
-    
-    public void quickSort(int[] nums, int left, int right){
-        if(left >= right) return; // Array of 0 or 1 element -> Already Sorted
-        
-        randomize(nums, left, right);
-        int pivot = partition(nums, left, right, nums[right]);
-        quickSort(nums, left, pivot - 1);
-        quickSort(nums, pivot + 1, right);
     }
 
     public int[] sortArray(int[] nums) {
-        quickSort(nums, 0, nums.length - 1);
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+        for(int i=0; i<nums.length; i++){
+            min = Math.min(min, nums[i]);
+            max = Math.max(max, nums[i]);
+        }
+        
+        countSort(nums, min, max);
         return nums;
     }
 }
