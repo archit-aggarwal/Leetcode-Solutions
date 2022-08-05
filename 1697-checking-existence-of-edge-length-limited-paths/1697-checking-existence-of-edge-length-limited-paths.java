227fd3f49ef2bd4db3790f3717084e900ab0abfe
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-08-05 23:44:14 +0530

    Time: 150 ms (53.43%), Space: 145.3 MB (35.62%) - LeetHub

--- 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.java
+++ 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.java
@@ -1,70 +1,75 @@
 class Solution {
-    static class DSU{
+    public static class Query implements Comparable<Query>{
+        int idx;
+        int a, b;
+        int threshold;
+        
+        Query(int idx, int a, int b, int threshold){
+            this.idx = idx;
+            this.a = a; 
+            this.b = b;
+            this.threshold = threshold;
+        }
+            
+        public int compareTo(Query other){
+            return this.threshold - other.threshold;
+        }
+    }
+    
+    static class DSU {
         int[] parent;
         int[] rank;
 
-        DSU(int n){
+        DSU(int n) {
             parent = new int[n];
             Arrays.fill(parent, -1);
-            
             rank = new int[n];
             Arrays.fill(rank, 1);
         }
 
-        public void union(int a, int b){
-            a = find(a);
-            b = find(b);
+        public void union(int a, int b) {
+            int pa = find(a);
+            int pb = find(b);
+            if (pa == pb)
+                return;
 
-            if(a == b) return;
-            if(rank[a] >= rank[b]){
-                rank[a] += rank[b];
-                parent[b] = a;
+            if (rank[pa] > rank[pb]) {
+                parent[pb] = pa;
+                rank[pa] += rank[pb];
             } else {
-                rank[b] += rank[a];
-                parent[a] = b;
+                parent[pa] = pb;
+                rank[pb] += rank[pa];
             }
         }
 
-        public int find(int a){
-            if(parent[a] == -1) return a;
+        public int find(int a) {
+            if (parent[a] == -1)
+                return a;
             return parent[a] = find(parent[a]);
         }
     }
-
-    static class Query implements Comparable<Query>{
-        int idx, src, nbr, limit;
-        Query(int idx, int src, int nbr, int limit){
-            this.idx = idx;
-            this.src = src;
-            this.nbr = nbr;
-            this.limit = limit;
-        }
-        
-        public int compareTo(Query other){
-            return this.limit - other.limit;
-        }
-    }
     
-    public boolean[] distanceLimitedPathsExist(int n, int[][] edges, int[][] queries){
-        List<Query> sortedQ = new ArrayList<>();
-        for(int i=0; i<queries.length; i++)
-            sortedQ.add(new Query(i, queries[i][0], queries[i][1], queries[i][2]));
-        Collections.sort(sortedQ);
+    public boolean[] distanceLimitedPathsExist(int n, int[][] edgeList, int[][] queries) {
+        List<Query> sortedQueries = new ArrayList<>();
+        for(int i=0; i<queries.length; i++){
+            sortedQueries.add(new Query(i, queries[i][0], queries[i][1], queries[i][2]));
+        }
+        Collections.sort(sortedQueries);
+        Arrays.sort(edgeList, (a, b) -> a[2] - b[2]);
         
-        Arrays.sort(edges, (e1, e2) -> e1[2] - e2[2]);
+        DSU sets = new DSU(n);
+        boolean[] res = new boolean[queries.length];
         int e = 0;
-        boolean[] ans = new boolean[queries.length];
-        DSU dsu = new DSU(n);
         
-        for(Query q: sortedQ){
-            while(e < edges.length && edges[e][2] < q.limit){
-                dsu.union(edges[e][0], edges[e][1]);
+        for(Query q: sortedQueries){
+            while(e < edgeList.length && edgeList[e][2] < q.threshold){
+                sets.union(edgeList[e][0], edgeList[e][1]);
                 e++;
             }
-            
-            if(dsu.find(q.src) == dsu.find(q.nbr)) ans[q.idx] = true;
-            else ans[q.idx] = false;
+            if(sets.find(q.a) == sets.find(q.b))
+                res[q.idx] = true;
         }
-        return ans;
+        
+        return res;
     }
 }
