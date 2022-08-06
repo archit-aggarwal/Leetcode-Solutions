9822bd412cbc362c0ef73d4bdaf1863c6e085c9c
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-08-06 12:05:35 +0530

    Time: 0 ms (100.00%), Space: 45.6 MB (90.45%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,42 +1,37 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] ans = new int[2];
-        ans[0] = ans[1] = -1;
+        int firstOcc = -1;
         
-        // First Occurence
         int left = 0, right = nums.length - 1;
         while(left <= right){
             int mid = left + (right - left) / 2;
             
             if(nums[mid] == target){
-                ans[0] = mid;
-                // Store the current occurence
+                firstOcc = mid;
                 right = mid - 1;
-                // continue the binary search to the left range
-            } else if(nums[mid] < target){
-                left = mid + 1;
+            } else if(nums[mid] > target){
+                right = mid - 1;
             } else {
-                right = mid - 1;
+                left = mid + 1;
             }
         }
         
-        // Last Occurence
+        
+        int lastOcc = -1;
         left = 0; right = nums.length - 1;
         while(left <= right){
             int mid = left + (right - left) / 2;
             
             if(nums[mid] == target){
-                ans[1] = mid;
-                // Store the current occurence
+                lastOcc = mid;
                 left = mid + 1;
-                // continue the binary search to the right range
-            } else if(nums[mid] < target){
-                left = mid + 1;
-            } else {
+            } else if(nums[mid] > target){
                 right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
         
-        return ans;
+        return new int[]{firstOcc, lastOcc};
     }
 }
