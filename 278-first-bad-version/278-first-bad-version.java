9de04c69092b0c9e5867ef6ac2596048a753c05c
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-08-06 12:36:41 +0530

    Time: 32 ms (16.04%), Space: 40.8 MB (59.42%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -3,19 +3,20 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int left = 1, right = n, ans = n;
-
+        int left = 1, right = n;
+        int ans = n;
+        
         while(left <= right){
             int mid = left + (right - left) / 2;
-
-            if(isBadVersion(mid) == false) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+            
+            if(isBadVersion(mid) == true){
                 ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
-
+        
         return ans;
     }
 }
