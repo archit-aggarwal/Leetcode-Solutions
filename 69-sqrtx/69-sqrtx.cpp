2f25b340f7c480ab4b1a9d5291c9335b7d7643d0
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-08-07 11:07:05 +0530

    Time: 8 ms (27.88%), Space: 5.9 MB (73.92%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,10 +1,29 @@
 class Solution {
 public:
     int mySqrt(int x) {
+        if(x <= 1) return x;
+        
+        // Linear Search = O(Root N) 
+        // long ans = 1l;
+        // while(ans * ans <= x){
+        //     ans++;
+        // }
+        // return (int)(ans - 1);
+        
+        // Binary Search
+        long left = 1l, right = x;
         long ans = 1l;
-        while(ans * ans <= x){
-            ans++;
+        
+        while(left <= right){
+            long mid = left + (right - left) / 2l;
+            if(mid * mid <= x){
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
-        return (int)(ans - 1);
+        
+        return (int)ans;
     }
 };
