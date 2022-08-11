c51e14083aebb2652a034b98c747467a499e0d7c
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-08-11 19:00:50 +0530

    Time: 6 ms (73.86%), Space: 75 MB (80.38%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,5 +1,6 @@
 class Solution {
-    public int findDuplicate(int[] nums) {
+    public int approach1(int[] nums){
+        // Approach 1: Using Count Sort
         int n = nums.length;
         
         for(int i=0; i<n; i++){
@@ -12,5 +13,27 @@ class Solution {
                 return (i + 1);
         }
         return (n + 1);
+    }
+    
+    public int approach2(int[] nums){
+        // Approach 2: Floyd Cycle
+        int slow = 0, fast = 0;
+        
+        while(true){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if(slow == fast) break;
+        }
+        
+        slow = 0;
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+    
+    public int findDuplicate(int[] nums) {
+        return approach2(nums);
     }
 }
