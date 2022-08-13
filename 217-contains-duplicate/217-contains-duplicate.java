81570a8c6484b52e9d47af55da38cc43b7db2af0
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-08-13 15:49:31 +0530

    Time: 15 ms (48.25%), Space: 70.8 MB (32.32%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> vis = new HashSet<>();
+        for(int val: nums){
+            if(vis.contains(val) == true) 
+                return true;
+            vis.add(val);
+        }
+        
+        return false;
+    }
+}
