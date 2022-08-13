9398356bff9cf90ed61fe822ffe7c314dbd08275
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-08-13 16:42:06 +0530

    Time: 56 ms (28.53%), Space: 42.8 MB (89.82%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,34 +1,32 @@
 class Solution {
     public int firstUniqChar(String s) {
         // Brute Force - Time - O(N * N)
-//         for(int i=0; i<s.length(); i++){
-//             char ch = s.charAt(i);
-            
-//             boolean isUnique = true;
-//             for(int j=0; j<s.length(); j++){
-//                 if(i != j && s.charAt(j) == ch){
-//                     isUnique = false;
-//                 }
-//             }
-            
-//             if(isUnique == true) return i;
-//         }
-        
-//         return -1;
-        
-        
-        int[] freq = new int[26];
-        
         for(int i=0; i<s.length(); i++){
-            int idx = s.charAt(i) - 'a';
-            freq[idx]++;
-        }
-        
-        for(int i=0; i<s.length(); i++){
-            int idx = s.charAt(i) - 'a';
-            if(freq[idx] == 1) return i;
+            boolean unique = true;
+            for(int j=0; j<s.length(); j++){
+                if(i != j && s.charAt(j) == s.charAt(i)){
+                    unique = false;
+                    break;
+                }
+            }
+            if(unique == true) return i;
         }
         
         return -1;
+        
+        
+//         int[] freq = new int[26];
+        
+//         for(int i=0; i<s.length(); i++){
+//             int idx = s.charAt(i) - 'a';
+//             freq[idx]++;
+//         }
+        
+//         for(int i=0; i<s.length(); i++){
+//             int idx = s.charAt(i) - 'a';
+//             if(freq[idx] == 1) return i;
+//         }
+        
+//         return -1;
     }
 }
