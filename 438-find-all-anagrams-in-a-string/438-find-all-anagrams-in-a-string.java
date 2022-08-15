2bad211b43d0f0857216b57d7e77494aa4c37a1c
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-08-15 12:31:58 +0530

    Time: 7 ms (95.71%), Space: 43.3 MB (94.02%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,24 +1,20 @@
 class Solution {
     public boolean isAnagram(int[] freq){
-        for(int i=0; i<26; i++){
-            if(freq[i] != 0) return false;
-        }    
+        for(int i=0; i<26; i++) if(freq[i] != 0) return false;
         return true;
     }
     
-    public List<Integer> findAnagrams(String a, String b) {
+    public List<Integer> findAnagrams(String s, String p) {
         int[] freq = new int[26];
-        for(int i=0; i<b.length(); i++)
-            freq[b.charAt(i) - 'a']++;
+        for(int i=0; i<p.length(); i++) freq[p.charAt(i) - 'a']--;
         
         List<Integer> res = new ArrayList<>();
-        for(int i=0; i<a.length(); i++){
-            freq[a.charAt(i) - 'a']--;
+        for(int r=0; r<s.length(); r++){
+            freq[s.charAt(r) - 'a']++; // Right -> Include
             
-            if(i >= b.length()) 
-                freq[a.charAt(i - b.length()) - 'a']++; 
-            if(isAnagram(freq) == true) 
-                res.add(i - b.length() + 1);
+            int l = r - p.length();
+            if(l >= 0) freq[s.charAt(l) - 'a']--; // Left -> Exclude 
+            if(isAnagram(freq) == true) res.add(l + 1);
         }
         
         return res;
