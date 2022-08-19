0ccdf9733a7e2ccf4d256c0b9841676726b0b611
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-08-19 18:08:14 +0530

    Time: 8 ms (5.43%), Space: 54.2 MB (39.52%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -0,0 +1,48 @@
+class Solution {
+    public void setRow(int[][] matrix, int row, int val){
+        for(int col=0; col<matrix[0].length; col++)
+            matrix[row][col] = val;
+    }
+    
+    public void setCol(int[][] matrix, int col, int val){
+        for(int row=0; row<matrix.length; row++)
+            matrix[row][col] = val;
+    }
+    
+//     public void approach1(int[][] matrix){
+//         // Inplace - O(1) space, if allowed to store other than 0/1
+//         for(int i=0; i<matrix.length; i++){
+//             for(int j=0; j<matrix[0].length; j++){
+//                 if(matrix[i][j] == 0){
+//                     setRow(matrix, i, -1);
+//                     setCol(matrix, j, -1);
+//                 }
+//             }
+//         }
+        
+//         for(int i=0; i<matrix.length; i++)
+//             for(int j=0; j<matrix[0].length; j++)
+//                 if(matrix[i][j] == -1)
+//                     matrix[i][j] = 0;
+//     }
+    
+    public void approach2(int[][] matrix){
+        // Extra Space - O(N * M) 
+        
+        int[][] copy = Arrays.stream(matrix).map(int[]::clone).toArray(int[][]::new);
+        // creating a copy of 2d matrix
+        
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(copy[i][j] == 0){
+                    setRow(matrix, i, 0);
+                    setCol(matrix, j, 0);
+                }
+            }
+        }
+    }
+    
+    public void setZeroes(int[][] matrix) {
+        approach2(matrix);
+    }
+}
