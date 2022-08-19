3c5f9b3fdce33bfa0b8b3d83576996212c3b7d91
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-08-19 16:41:22 +0530

    Time: 2 ms (89.40%), Space: 56.9 MB (16.34%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -2,13 +2,10 @@ class Solution {
     public int majorityElement(int[] nums) {
         int majority = Integer.MIN_VALUE, freq = 0;
         for(int val: nums){
+            if(freq <= 0) majority = val;
+            
             if(val == majority) freq++;
             else freq--;
-            
-            if(freq <= 0){
-                majority = val;
-                freq = 1;
-            }
         }
         
         return majority;
