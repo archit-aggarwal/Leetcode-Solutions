4c700d2fba93149bfaf849944d3e1ac3ea3974e7
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-08-19 18:47:23 +0530

    Time: 1 ms (97.61%), Space: 54 MB (66.37%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -65,9 +65,43 @@ class Solution {
         }
     }
     
-    
+    public void approach4(int[][] matrix){
+        // Extra Space - O(N + M), Time = O(N * M)
+        
+        boolean row0 = false;
+        for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(matrix[i][j] == 0){
+                    if(i == 0) row0 = true;
+                    else matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        for(int i=1; i<matrix.length; i++){
+            for(int j=1; j<matrix[0].length; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        if(matrix[0][0] == 0){
+            for(int i=0; i<matrix.length; i++){
+                matrix[i][0] = 0;
+            }
+        }
+        
+        if(row0 == true){
+            for(int j=0; j<matrix[0].length; j++){
+                matrix[0][j] = 0;
+            }
+        }
+        
+        
+    }
     
     public void setZeroes(int[][] matrix) {
-        approach3(matrix);
+        approach4(matrix);
     }
 }
