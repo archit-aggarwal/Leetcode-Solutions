227067be02cb05c31e66d85cd3add7fddf3e26a9
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-08-20 17:46:51 +0530

    Time: 67 ms (84.63%), Space: 103.5 MB (65.59%) - LeetHub

--- 834-sum-of-distances-in-tree/834-sum-of-distances-in-tree.java
+++ 834-sum-of-distances-in-tree/834-sum-of-distances-in-tree.java
@@ -1,42 +1,43 @@
 class Solution {
-    int[] sum, count;
+    int[] size, ans;
     
-    public void DFS1(int src, int par, ArrayList<Integer>[] adj){
-        count[src] = 1;
-        
+    public void subtree(int src, int par, ArrayList<Integer>[] adj){
         for(Integer nbr: adj[src]){
             if(nbr == par) continue;
-            DFS1(nbr, src, adj);
-            count[src] += count[nbr];
-            sum[src] += (sum[nbr] + count[nbr]);
-        }    
+            subtree(nbr, src, adj);
+            
+            ans[src] += (ans[nbr] + size[nbr]);
+            size[src] += size[nbr];
+        }
     }
     
-    public void DFS2(int src, int par, ArrayList<Integer>[] adj){
+    public void dfs(int src, int par, ArrayList<Integer>[] adj){
         if(par != -1){
-            sum[src] = sum[par] - count[src] + (adj.length - count[src]);    
+            ans[src] = ans[par] + adj.length - 2 * size[src];
         }
         
-        for(Integer nbr: adj[src]){
+         for(Integer nbr: adj[src]){
             if(nbr == par) continue;
-            DFS2(nbr, src, adj);
-        }  
+            dfs(nbr, src, adj);
+        }
     }
     
     public int[] sumOfDistancesInTree(int n, int[][] edges) {
-        sum = new int[n];
-        count = new int[n];
+        size = new int[n];
+        Arrays.fill(size, 1);
         
         ArrayList<Integer>[] adj = new ArrayList[n];
-        for(int i=0; i<n; i++) adj[i] = new ArrayList<>();
-        
-        for(int[] edge: edges){
+        for (int i = 0; i < n; i++)
+            adj[i] = new ArrayList<>();
+
+        for(int[] edge: edges) {
             adj[edge[0]].add(edge[1]);
             adj[edge[1]].add(edge[0]);
         }
         
-        DFS1(0, -1, adj);
-        DFS2(0, -1, adj);
-        return sum;
+        ans = new int[n];
+        subtree(0, -1, adj);
+        dfs(0, -1, adj);
+        return ans;
     }
 }
