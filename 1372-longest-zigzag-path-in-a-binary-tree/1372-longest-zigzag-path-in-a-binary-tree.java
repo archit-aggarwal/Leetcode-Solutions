36bfa69537894b16055c5d2a92b79d6d03cc687a
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-08-21 16:46:49 +0530

    Time: 13 ms (49.65%), Space: 71.5 MB (74.12%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.java
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.java
@@ -16,37 +16,37 @@
 
 class Solution {
     public static class Pair{
-        int max, left, right;
-        Pair(int max, int left, int right){
-            this.max = max;
+        int left, right;
+        Pair(int left, int right){
             this.left = left;
             this.right = right;
         }
     }
+    int max = 0;
     
     public Pair dfs(TreeNode root){
-        if(root == null) return new Pair(0, 0, 0);
+        if(root == null) return new Pair(0, 0);
         
-        Pair curr = new Pair(0, 0, 0);
+        Pair curr = new Pair(0, 0);
         
         if(root.left != null){
             Pair left = dfs(root.left);
             curr.left = 1 + left.right;
-            curr.max = Math.max(curr.max, curr.left);
-            curr.max = Math.max(curr.max, left.max);
+            max = Math.max(max, curr.left);
         } 
         
         if(root.right != null){
             Pair right = dfs(root.right);
             curr.right = 1 + right.left;
-            curr.max = Math.max(curr.max, curr.right);
-            curr.max = Math.max(curr.max, right.max);
+            max = Math.max(max, curr.right);
         }
         
         return curr;
     }
     
     public int longestZigZag(TreeNode root) {
-        return dfs(root).max;
+        max = 0;
+        dfs(root);
+        return max;
     }
 }
