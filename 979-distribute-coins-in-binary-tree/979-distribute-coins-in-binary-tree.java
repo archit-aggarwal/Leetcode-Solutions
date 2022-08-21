1c7bc68ff40afaed6272293f25c9e914c46f691f
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-08-21 18:07:34 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (75.72%) - LeetHub

--- 979-distribute-coins-in-binary-tree/979-distribute-coins-in-binary-tree.java
+++ 979-distribute-coins-in-binary-tree/979-distribute-coins-in-binary-tree.java
@@ -1,44 +1,22 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
-    public static class Pair{
-        int coins;
-        int size;
-        Pair(int coins, int size){
-            this.coins = coins;
-            this.size = size;
-        }
-    }
+    int operations = 0;
     
-    int ans;
-    public Pair dfs(TreeNode root){
-        if(root == null) return new Pair(0, 0);
+    public int dfs(TreeNode root){
+        if(root == null) return 0;
         
-        Pair left = dfs(root.left);
-        Pair right = dfs(root.right);
-        ans = ans + Math.abs(left.coins - left.size)
-                  + Math.abs(right.coins - right.size);
+        int left = dfs(root.left);
+        int right = dfs(root.right);
         
-        return new Pair(root.val + left.coins + right.coins, 
-                        1 + left.size + right.size);
+        operations += Math.abs(left) + Math.abs(right);
+        
+        // (root.coins - root.size) = (left.coins - left.size) + (right.coins - right.size) + (root.val - 1)
+        return left + right + (root.val - 1);
     }
     
     public int distributeCoins(TreeNode root) {
-        ans = 0;
+        operations = 0;
         dfs(root);
-        return ans;
+        return operations;
     }
 }
