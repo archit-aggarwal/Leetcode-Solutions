afaf52a1bb925b52462505ca0ab5b377e1fb93c9
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-08-26 12:35:19 +0530

    Time: 9 ms (82.79%), Space: 42.3 MB (45.08%) - LeetHub

--- 1116-print-zero-even-odd/1116-print-zero-even-odd.java
+++ 1116-print-zero-even-odd/1116-print-zero-even-odd.java
@@ -8,38 +8,32 @@ class ZeroEvenOdd {
     }
 
     // printNumber.accept(x) outputs "x", where x is an integer.
-    public void zero(IntConsumer printNumber) throws InterruptedException {
+    public synchronized void zero(IntConsumer printNumber) throws InterruptedException {
         while(ptr <= n){
-            synchronized(this){ 
-                while(zeroLock == true) wait();
-                if(ptr <= n) printNumber.accept(0);
-                zeroLock = true;
-                notifyAll();
-            }
+            while(zeroLock == true) wait();
+            if(ptr <= n) printNumber.accept(0);
+            zeroLock = true;
+            notifyAll();
         }
     }
 
-    public void even(IntConsumer printNumber) throws InterruptedException {
+    public synchronized void even(IntConsumer printNumber) throws InterruptedException {
         while(ptr <= n){
-            synchronized(this){ 
-                while(zeroLock == false || ptr % 2 == 1) wait();
-                if(ptr <= n) printNumber.accept(ptr);
-                zeroLock = false;
-                ptr++;
-                notifyAll(); 
-            }
+            while(zeroLock == false || ptr % 2 == 1) wait();
+            if(ptr <= n) printNumber.accept(ptr);
+            zeroLock = false;
+            ptr++;
+            notifyAll(); 
         }
     }
 
-    public void odd(IntConsumer printNumber) throws InterruptedException {
+    public synchronized void odd(IntConsumer printNumber) throws InterruptedException {
         while(ptr <= n){
-            synchronized(this){ 
-                while(zeroLock == false || ptr % 2 == 0) wait();
-                if(ptr <= n) printNumber.accept(ptr);
-                zeroLock = false;
-                ptr++;
-                notifyAll(); 
-            }
+            while(zeroLock == false || ptr % 2 == 0) wait();
+            if(ptr <= n) printNumber.accept(ptr);
+            zeroLock = false;
+            ptr++;
+            notifyAll(); 
         }
     }
 }
