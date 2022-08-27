88d5b7e80918652c86bac0e3e1a9e2f2a40333c0
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-08-27 10:58:14 +0530

    Time: 1 ms (100.00%), Space: 44.3 MB (90.24%) - LeetHub

---
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortedMerge(ListNode head1, ListNode head2) {
+           ListNode head = new ListNode(-1);
+           ListNode tail = head;
+
+           while(head1 != null && head2 != null){
+               if(head1.val <= head2.val){
+                   tail.next = head1;
+                   tail = tail.next;
+                   head1 = head1.next;
+               } else {
+                   tail.next = head2;
+                   tail = tail.next;
+                   head2 = head2.next;
+               }
+           }
+
+           if(head1 != null) tail.next = head1;
+           else tail.next = head2;
+
+           return head.next;
+     } 
+    
+    public ListNode helper(ListNode[] lists, int left, int right){
+        if(left > right) return null; 
+        if(left == right) return lists[left];
+        
+        int mid = left + (right - left) / 2;
+        
+        ListNode leftHead = helper(lists, left, mid);
+        ListNode rightHead = helper(lists, mid + 1, right);
+        
+        return sortedMerge(leftHead, rightHead);
+    }
+    
+    public ListNode mergeKLists(ListNode[] lists) {
+        return helper(lists, 0, lists.length - 1);    
+    }
+}
