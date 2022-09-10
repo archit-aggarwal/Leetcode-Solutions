90889392a726bc4bf3f2af0522b1308c627d8bac
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-09-10 11:30:14 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (15.31%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -1,31 +1,15 @@
 class Solution {
+    List<Integer> inorder = new ArrayList<>();
+    
+    public void dfs(TreeNode root){
+        if(root == null) return;
+        dfs(root.left);
+        inorder.add(root.val);
+        dfs(root.right);
+    }
+    
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> inorder = new ArrayList<>();
-        
-        while(root != null){
-            if(root.left == null) {
-                inorder.add(root.val);
-                root = root.right;
-                continue;
-            }
-            
-            TreeNode rightMost = root.left;
-            while(rightMost.right != null && rightMost.right != root){
-                rightMost = rightMost.right;
-            }
-            
-            if(rightMost.right == null){
-               // left subtree is not visited
-               rightMost.right = root;
-               root = root.left;
-            } else {
-                // left subtree is visited
-                inorder.add(root.val);
-                rightMost.right = null; 
-                root = root.right;
-            }
-        }
-        
+        dfs(root);
         return inorder;
     }
 }
