5a669a233da690294b6a1f2c0e5ed8fb3b2791c9
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-10-29 11:08:40 +0530

    Added solution - LeetHub

---
+++ Binary Array Sorting - GFG/binary-array-sorting.java
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Sorting
+{
+    public static void main(String args[])throws IOException
+    {
+        // Scanner sc = new Scanner(System.in);
+        // int t = sc.nextInt();
+        
+        //reading input using BufferedReader class
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+        //reading testcases count
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)       {
+            
+            String st[] = read.readLine().trim().split(" ");
+            
+            //reading size of array 
+            int N =Integer.parseInt(st[0]);
+            
+            //creating a new array of size n
+            int A[] = new int[N];
+            st = read.readLine().trim().split(" ");
+            
+            //adding elements to the array
+            for(int i = 0; i < N; i++)
+                A[i] = Integer.parseInt(st[i]);
+                
+            //calling the method binSort() of
+            //class BinarySort()
+            new Solution().binSort(A, N);
+            
+            //printing the elements of array
+            for(Integer x: A)
+                System.out.print(x + " ");
+            System.out.println();
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    static void swap(int[] arr, int left, int right){
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+    }
+    static void binSort(int arr[], int N)
+    {
+        int left = 0, right = 0;
+        while(right < N){
+            if(arr[right] == 0){
+                swap(arr, left, right);
+                left++; right++;
+            } else {
+                right++;
+            }
+        }
+    }
+}
+
