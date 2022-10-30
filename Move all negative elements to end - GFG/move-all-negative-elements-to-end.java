79e8d82e7e4249ce57eacd00f7a8046ee6b1ba31
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-10-30 11:41:36 +0530

    Added solution - LeetHub

--- Move all negative elements to end - GFG/move-all-negative-elements-to-end.java
+++ Move all negative elements to end - GFG/move-all-negative-elements-to-end.java
@@ -38,8 +38,7 @@ class GFG {
 //User function Template for Java
 
 class Solution {
-    
-    public void approach1(int[] arr){
+    public void twoTraversalExtraSpace(int[] arr){
         int[] res = new int[arr.length]; 
         
         int left = 0;
@@ -63,8 +62,57 @@ class Solution {
         }
     }
     
-    public void segregateElements(int arr[], int n)
-    {
-        approach1(arr);
+    public void swap(int[] arr, int p1, int p2){
+        int temp = arr[p1];
+        arr[p1] = arr[p2];
+        arr[p2] = temp;
+    }
+    
+    public void insertionSort(int[] arr){
+        for(int i = 1; i < arr.length; i++){
+            if(arr[i] < 0) continue;
+            
+            for(int j = i - 1; j >= 0; j--){
+                if(arr[j] < 0){
+                    swap(arr, j, j + 1);
+                } else break;
+            }
+        }
+    }
+    
+    public void reverse(int[] arr, int left, int right){
+        while(left < right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++; right--;
+        }    
+    }
+    
+    public void merge(int[] arr, int left, int mid, int right){
+        int p1 = left; // p1 is first negative element in left array
+        while(p1 <= mid && arr[p1] >= 0) p1++;
+        
+        int p2 = mid + 1; // p2 is first negative element in right array
+        while(p2 <= right && arr[p2] >= 0) p2++;
+        
+        reverse(arr, p1, mid);
+        reverse(arr, mid + 1, p2 - 1);
+        reverse(arr, p1, p2 - 1);
+    }
+    
+    public void mergeSort(int[] arr, int left, int right){
+        if(left == right) return;
+        
+        int mid = (left + right) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+    
+    public void segregateElements(int arr[], int n) {
+        // approach1(arr);
+        // approach2(arr);
+        mergeSort(arr, 0, n - 1);
     }
 }
