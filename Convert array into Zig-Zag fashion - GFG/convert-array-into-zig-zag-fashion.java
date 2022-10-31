5699362f29d9e679cf868f8f6de7f294d10941eb
Archit Aggarwal <archit.aggarwal023@gmail.com>
2022-10-31 23:37:29 +0530

    Added solution - LeetHub

---
+++ Convert array into Zig-Zag fashion - GFG/convert-array-into-zig-zag-fashion.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+  public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().zigZag(arr, n);
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public void swap(int[] arr, int p1, int p2){
+        int temp = arr[p1];
+        arr[p1] = arr[p2];
+        arr[p2] = temp;
+    }
+    
+    void zigZag(int arr[], int n) {
+        for(int idx = 0; idx < n; idx = idx + 2){ // valley indices
+            if(idx - 1 >= 0 && arr[idx - 1] < arr[idx]){
+                swap(arr, idx - 1, idx);
+            }
+            
+            if(idx + 1 < n && arr[idx + 1] < arr[idx]){
+                swap(arr, idx + 1, idx);
+            }
+        }
+    }
+}
